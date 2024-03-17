a7224f7126ffa19c669ba6a844c7d341d16fdff3
mplaczek99 <mplaczek99@proton.me>
2024-03-17 16:28:25 -0500

    Added JavaDocs for the methods in Cart.java, will help us later

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -12,6 +12,12 @@ public class Cart {
     LOGGER = Logger.getLogger(Cart.class.getName());
   }
 
+  /**
+   * Adds an item to the cart, and updates the quantity if it exists
+   *
+   * @param product Product to add
+   * @param quantity Quantity to add
+   */
   public void addItem(final Product product, final int quantity) {
     items.stream()
         .filter(item -> item.getProduct().equals(product))
@@ -27,6 +33,11 @@ public class Cart {
             });
   }
 
+  /**
+   * Removes an item from the cart
+   *
+   * @param product Product to remove
+   */
   public void removeItem(final Product product) {
     boolean removed = items.removeIf(item -> item.getProduct().equals(product));
     if (removed) {
@@ -36,6 +47,11 @@ public class Cart {
     }
   }
 
+  /**
+   * Returns the singleton instance of the cart
+   * 
+   * @return The singleton instance
+   */
   public static synchronized Cart getInstance() { // synchronized for thread-safety
     if (instance == null) {
       instance = new Cart();
@@ -43,16 +59,29 @@ public class Cart {
     return instance;
   }
 
+  /**
+   * Calculates the total price of all items in the cart
+   *
+   * @return The total price of all items
+   */
   public double getTotalPrice() {
     return items.stream()
         .mapToDouble(item -> item.getProduct().getPrice() * item.getQuantity())
         .sum();
   }
 
+  /**
+   * Returns the list of items in the cart
+   *
+   * @return The list of items
+   */
   public List<Item> getItems() {
     return new ArrayList<>(items);
   }
 
+  /**
+   * Removes all items from the cart
+   */
   public void clearItems() {
     items.clear();
   }
@@ -63,19 +92,37 @@ class CartBuilder {
 
   public CartBuilder() {
     this.cart = Cart.getInstance();
-    this.cart.clearItems(); // Ensure the cart is empty before building
+    this.cart.clearItems(); // Ensures the cart is empty before building
   }
 
+  /**
+   * Adds an item to the cart
+   *
+   * @param product Product to add
+   * @param quantity Quantity to add
+   * @return The CartBuilder
+   */
   public CartBuilder addItem(final Product product, final int quantity) {
     cart.addItem(product, quantity);
     return this;
   }
 
+  /**
+   * Removes an item from the cart
+   *
+   * @param product Product to remove
+   * @return The CartBuilder
+   */
   public CartBuilder removeItem(final Product product) {
     cart.removeItem(product);
     return this;
   }
 
+  /**
+   * Builds the cart
+   *
+   * @return The Cart
+   */
   public Cart build() {
     return cart;
   }
