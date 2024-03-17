cd06fb39c7eeb2fb96ca29d84a2d2b880d2f39c2
mplaczek99 <mplaczek99@proton.me>
2024-03-17 16:40:20 -0500

    Added JavaDocs for the methods in Catalog.java, will help us later

--- src/main/java/Catalog.java
+++ src/main/java/Catalog.java
@@ -4,18 +4,33 @@ import java.util.List;
 public class Catalog {
   private final List<Product> products = new ArrayList<>();
 
+  /**
+   * Adds a product to the catalog
+   *
+   * @param product Product to add
+   */
   public void addProduct(final Product product) {
     if (!products.contains(product)) {
       products.add(product);
     }
   }
 
+  /**
+   * Removes a product from the catalog
+   *
+   * @param product Product to remove
+   */
   public void removeProduct(final Product product) {
     if (products.contains(product)) {
       products.remove(product);
     }
   }
 
+  /**
+   * Returns the list of products in the catalog
+   *
+   * @return The list of products
+   */
   public List<Product> getProducts() {
     return new ArrayList<>(products);
   }
